75eafffcbee3ac66365804347d2481cd31c287c4
Langfton <langtonfavor@gmail.com>
2019-03-08 16:02:08 +0200

    started with react

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Link from 'react-router-dom';
+import Navbar from 'react-bootstrap';
+
+import App from './app.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  render(){
+    return(
+        <div className="App Container">
+          <Navbar fluid collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">From Scratch</Link>
+              </Navbar.Brand>
+              <Navbar.Toggle/>
+            </Navbar.Header>
+          </Navbar>
+        </div>
+            );
+}
 }
 
 export default App;
--- notes-app-client/src/index.js
+++ notes-app-client/src/index.js
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
 
-ReactDOM.render(
-<Router>
-<App />
-</Router>,
-document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
